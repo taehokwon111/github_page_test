99bb5b6aacf611f0a268a5afb4b2c3c0c110b019
cancer maker <52158699+Cancermaker@users.noreply.github.com>
2021-04-11 06:38:29 +0900

    add

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ x_end, y_end, bar_height, bar_width, audio_f, audio, now_load, files,
 frequency_array;
 
 bars = 150;
-bar_width = 5;
+bar_width = 1;
 
 
 function handleFiles(event) {
@@ -55,7 +55,12 @@ ctx = canvas.getContext("2d");
 // 창의 중심을 찾아서 크기조절
 center_x = canvas.width / 2;
 center_y = canvas.height / 2;
-radius = 200;
+if(window.innerWidth > 300){
+    radius = 200;
+} else{
+
+    radius = center_x / 2;
+}
 
 // 배경스타일 지정
 let gradient = ctx.createLinearGradient(0,0,0,canvas.height);
@@ -95,7 +100,7 @@ window.requestAnimationFrame(animationLooper);
 // 막대그리기
 function drawBar(x1, y1, x2, y2, width, frequency){
 
-let lineColor = `rgb(${0},${21}, ${frequency} )`;
+let lineColor = `rgb(${frequency+2},${frequency+2}, ${frequency+2} )`;
 // "rgb(" + 0 + ", " + 50 + ", " + frequency + ")"
 ctx.strokeStyle = lineColor;
 ctx.lineWidth = width;
